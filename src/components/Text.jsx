53d1aa5f942cf0eaf799825e51153435409dc657
Adiletkk <adilet_esirkepov02@mail.ru>
2023-04-05 16:57:38 +0500

    create/file

---
+++ src/components/Text.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import css from './text.module.scss'
+
+function text() {
+  return (
+    <div>text</div>
+  )
+}
+
+export default text
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import css from './main.module.scss'
+
+function Main() {
+  return (
+    <div className={css.wrapper}>main</div>
+  )
+}
+
+export default Main
