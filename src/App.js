df14b4fdca6fb776d59f4f39083f4362d2ba039c
MAXONCHIK236 <m-garzhaikin@bk.ru>
2023-04-19 15:26:32 +0300

    fix/github

--- src/App.js
+++ src/App.js
@@ -1,48 +1,33 @@
-import './App.css';
-import TravelList from './components/ComponentPage2/TravelList/TravelList';
-import Explore from './components/ComponentsPage1/ExploreDifferent/Explore';
-import FastWay from './components/ComponentsPage1/FastestWay/FastWay';
-import CardFeature from './components/ComponentsPage1/FeaturedTours/Cards/CardFeatureTour';
-import FeaturTour from './components/ComponentsPage1/FeaturedTours/FeaturTour';
-import Footer from './components/ComponentsPage1/Footer/Footer';
-import LatestPost from './components/ComponentsPage1/LatestPosts/LatePost';
-import OurFavorite from './components/ComponentsPage1/OurFavoriteDestinations/OurFavorite';
-import Thailand from './components/ComponentsPage1/ThailandTour/Thailand';
-import TourCityCard from './components/ComponentsPage1/ToursByCity/Cards/TourCityCard';
-import TourCity from './components/ComponentsPage1/ToursByCity/TourCity';
+import "./App.css";
+import TravelList from "./components/ComponentPage2/TravelList/TravelList";
+import Explore from "./components/ComponentsPage1/ExploreDifferent/Explore";
+import FastWay from "./components/ComponentsPage1/FastestWay/FastWay";
+import CardFeature from "./components/ComponentsPage1/FeaturedTours/Cards/CardFeatureTour";
+import FeaturTour from "./components/ComponentsPage1/FeaturedTours/FeaturTour";
+// import Footer from './components/ComponentsPage1/Footer/Footer';
+import LatestPost from "./components/ComponentsPage1/LatestPosts/LatePost";
+import OurFavorite from "./components/ComponentsPage1/OurFavoriteDestinations/OurFavorite";
+import Thailand from "./components/ComponentsPage1/ThailandTour/Thailand";
+import TourCityCard from "./components/ComponentsPage1/ToursByCity/Cards/TourCityCard";
+import TourCity from "./components/ComponentsPage1/ToursByCity/TourCity";
 import "react-alice-carousel/lib/alice-carousel.css";
 import "react-alice-carousel/lib/scss/alice-carousel.scss";
 
-
-
-
 function App() {
   return (
-
-    <Routes>
-      <Route path="/" element={
-        <HomePage />
-      } />
-      <Route path="/Header" element={
-        <HeaderPage />
-      } />
-      <Route path="/paypage" element={
-        <PayPage />
-      } />
-      <Route path="/Regist" element={
-        <RegistrationPage />
-      } />
-      <Route path="/Slider" element={
-        <SliderPage />
-      } />
-      <Route path="/docum" element={
-        <Documentation />
-      } />
-      <Route path='/contact' element={<ContactPage />} />
-      <Route path="/about" element={<AboutPage/>} />
-    </Routes>
-   
-
+    <div>
+      <TravelList />
+    </div>
+    // <Routes>
+    //   {/* <Route path="/" element={<HomePage />} />
+    //   <Route path="/Header" element={<HeaderPage />} />
+    //   <Route path="/paypage" element={<PayPage />} />
+    //   <Route path="/Regist" element={<RegistrationPage />} />
+    //   <Route path="/Slider" element={<SliderPage />} />
+    //   <Route path="/docum" element={<Documentation />} />
+    //   <Route path="/contact" element={<ContactPage />} />
+    //   <Route path="/about" element={<AboutPage />} /> */}
+    // {/* </Routes> */}
   );
 }
 
--- src/components/ComponentPage2/TravelList/TravelList.jsx
+++ src/components/ComponentPage2/TravelList/TravelList.jsx
@@ -5,7 +5,7 @@ import styles from "./Travel.module.scss";
 import layer from "./buttonimg/layer.svg";
 import OVERLAY from "./buttonimg/OVERLAY COLOR.svg";
 import CardFeature from "../../ComponentsPage1/FeaturedTours/Cards/CardFeatureTour";
-import Footer from "../../ComponentsPage1/Footer/Footer";
+// import Footer from "../../ComponentsPage1/Footer/Footer";
 import FirstCard from "./BgImg/ImageRatio1.svg";
 import SecondCard from "./BgImg/ImageRatio2.svg";
 import ThirdCard from "./BgImg/ImageRatio3.svg";
@@ -18,9 +18,9 @@ import NineCard from "./BgImg/ImageRatio9.svg";
 import TenCard from "./BgImg/ImageRatio10.svg";
 import ElevenCard from "./BgImg/ImageRatio11.svg";
 import TwelveCard from "./BgImg/ImageRatio12.svg";
-import ReactDOM from "react-dom";
-import ReactPaginate from "react-paginate";
-import { Pagination } from "@mui/material";
+// import ReactDOM from "react-dom";
+// import ReactPaginate from "react-paginate";
+// import { Pagination } from "@mui/material";
 function TravelList() {
   const settings = {
     dots: true,
@@ -28,7 +28,7 @@ function TravelList() {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
   };
   return (
     <>
@@ -52,97 +52,61 @@ function TravelList() {
             <img src={OVERLAY} alt="Guests" />
           </button>
         </div>
-        
-         <Slider className={styles.Slider} {...settings}>
-            <div className={styles.TravelListCard}>
-              <CardFeature
-                ImageCardFeature={FirstCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={SecondCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={ThirdCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={FourthCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={FiveCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature ImageCardFeature={SixCard} title="Warsaw Day Tour" />
-              <CardFeature
-                ImageCardFeature={SevenCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={EightCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={NineCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature ImageCardFeature={TenCard} title="Warsaw Day Tour" />
-              <CardFeature
-                ImageCardFeature={ElevenCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={TwelveCard}
-                title="Warsaw Day Tour"
-              />
-            </div>
-            <div className={styles.TravelListCard}>
-              <CardFeature
-                ImageCardFeature={FirstCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={SecondCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={ThirdCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={FourthCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={FiveCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature ImageCardFeature={SixCard} title="Warsaw Day Tour" />
-              <CardFeature
-                ImageCardFeature={SevenCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={EightCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={NineCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature ImageCardFeature={TenCard} title="Warsaw Day Tour" />
-              <CardFeature
-                ImageCardFeature={ElevenCard}
-                title="Warsaw Day Tour"
-              />
-              <CardFeature
-                ImageCardFeature={TwelveCard}
-                title="Warsaw Day Tour"
-              />
-            </div>
-            {/* <div className={styles.TravelListCard}>
+
+        <Slider className={styles.Slider} {...settings}>
+          <div className={styles.TravelListCard}>
+            <CardFeature ImageCardFeature={FirstCard} title="Warsaw Day Tour" />
+            <CardFeature
+              ImageCardFeature={SecondCard}
+              title="Warsaw Day Tour"
+            />
+            <CardFeature ImageCardFeature={ThirdCard} title="Warsaw Day Tour" />
+            <CardFeature
+              ImageCardFeature={FourthCard}
+              title="Warsaw Day Tour"
+            />
+            <CardFeature ImageCardFeature={FiveCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={SixCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={SevenCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={EightCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={NineCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={TenCard} title="Warsaw Day Tour" />
+            <CardFeature
+              ImageCardFeature={ElevenCard}
+              title="Warsaw Day Tour"
+            />
+            <CardFeature
+              ImageCardFeature={TwelveCard}
+              title="Warsaw Day Tour"
+            />
+          </div>
+          <div className={styles.TravelListCard}>
+            <CardFeature ImageCardFeature={FirstCard} title="Warsaw Day Tour" />
+            <CardFeature
+              ImageCardFeature={SecondCard}
+              title="Warsaw Day Tour"
+            />
+            <CardFeature ImageCardFeature={ThirdCard} title="Warsaw Day Tour" />
+            <CardFeature
+              ImageCardFeature={FourthCard}
+              title="Warsaw Day Tour"
+            />
+            <CardFeature ImageCardFeature={FiveCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={SixCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={SevenCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={EightCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={NineCard} title="Warsaw Day Tour" />
+            <CardFeature ImageCardFeature={TenCard} title="Warsaw Day Tour" />
+            <CardFeature
+              ImageCardFeature={ElevenCard}
+              title="Warsaw Day Tour"
+            />
+            <CardFeature
+              ImageCardFeature={TwelveCard}
+              title="Warsaw Day Tour"
+            />
+          </div>
+          {/* <div className={styles.TravelListCard}>
               <CardFeature
                 ImageCardFeature={FirstCard}
                 title="Warsaw Day Tour"
@@ -186,10 +150,9 @@ function TravelList() {
                 title="Warsaw Day Tour"
               />
             </div> */}
-            </Slider>
-     
+        </Slider>
       </section>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
