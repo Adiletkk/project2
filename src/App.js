8d1bdcdd19a8006590686147be794344eeb2f2da
Adiletkk <adilet_esirkepov02@mail.ru>
2023-04-19 16:25:20 +0500

    create-about page

--- src/App.js
+++ src/App.js
@@ -6,15 +6,8 @@ import PayPage from './Pages/PayPage/PayPage';
 import RegistrationPage from './Pages/RegistrationPage/RegistrationPage';
 import SliderPage from './Pages/SliderPage/SliderPage';
 import Documentation from './Pages/DocumentationPage/Documentation';
-import Card from './components/Card/Card';
-import Header from './components/Header/Header';
-import WeWork from './components/WeWork/WeWork';
-import Drop from './components/Drop/Drop';
-import AboutUs from './components/AboutUs/AboutUs';
-import Maecenas from './components/Maecenas/Maecenas';
-import OurMission from './components/OurMission/OurMission';
-import OurTeam from './components/OurTeam/OurTeam';
-import Footer from './components/ComponentsPage1/Footer/Footer';
+import Payend from './Pages/PayendPage/Payend';
+import About from './Pages/About/About';
 
 
 function App() {
@@ -26,13 +19,13 @@ function App() {
     <Route path="/" element={ 
       <HomePage/> 
     } /> 
-    <Route path="/Header" element={ 
+    <Route path="/header" element={ 
       <HeaderPage/> 
     }/> 
     <Route path="/paypage" element={ 
       <PayPage/> 
     }/> 
-    <Route path="/Regist" element={ 
+    <Route path="/regist" element={ 
       <RegistrationPage/> 
     }/> 
     <Route path="/Slider" element={ 
@@ -41,12 +34,11 @@ function App() {
      <Route path="/docum" element={ 
       <Documentation/> 
     }/> 
-    {/* <Route path="/contact" element={ 
-      <Contact/> 
+    <Route path="/payend" element={ 
+      <Payend/> 
     }/> 
     <Route path="/about" element={ 
-      <About/> 
-    }/>  */}
+      <About/> }/>  
     </Routes>
     </div>
   );
